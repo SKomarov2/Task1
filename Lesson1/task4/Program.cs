7613bed6d94923498449ebdbc7df4a98f44e6b2b
SKomarov <Komarov.Sergey.sk@yandex.ru>
2023-12-25 18:12:19 +0300

    add Lesson1\task4

---
+++ Lesson1/task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 10;
+int b = 182;
+int c = 5;
+int d = 15;
+int e = 11000;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
